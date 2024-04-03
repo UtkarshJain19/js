8ea7f587ff54564b11fd241de560f63155981ad6
UtkarshJain19 <162026620+UtkarshJain19@users.noreply.github.com>
2024-04-03 10:49:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+let id = 1
+let name = "Utkarsh"
+let isOnline = true
+let state;
+
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = 
+// symbols = unique
+
+
+// object
+
+console.log(typeof 123);
+console.log(typeof id);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
