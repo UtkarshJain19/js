e7e2300589d07fdb9bf91643c9a6e5012bdd5eda
UtkarshJain19 <162026620+UtkarshJain19@users.noreply.github.com>
2024-04-03 10:26:18 +0000

    learning varaibles

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 12375
+let accountEmail = "dhf@gmail.com"
+var accountPassword = "1234322"
+accountCity = "Pnp"
+let accountState;
+
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState]);
