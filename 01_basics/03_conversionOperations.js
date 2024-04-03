f2037a94dd9c554569ecba200b85fa0418872a42
UtkarshJain19 <162026620+UtkarshJain19@users.noreply.github.com>
2024-04-03 19:40:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+// let score = "52"
+
+// console.log(score);
+// console.log(typeof score);
+
+// let isNumber = Number(score)
+// console.log(typeof isNumber);
+// console.log(isNumber);
+
+let score = "52asd"
+
+console.log(score);
+console.log(typeof score);
+
+let isNumber = Number(score)
+console.log(typeof isNumber);
+console.log(isNumber);
+
+//"52" => 52
+//"52asd" => NaN
+// ture => 1
+// null => 0
+// undefined => NaN
+
+// when converting to boolean
+// 1 => true
+// "" => false
+// "utk" => true
